a33a10afa85043115ffb1ae05bc89450424fc40b
Cedric Porter <cedricporter@gmail.com>
2017-05-08 16:49:34 +0800

    bug fix

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -sf $(PWD)/.zshrc ~
-ln -sf $(PWD)/.tmux.conf ~
+ln -sf $PWD/.zshrc ~
+ln -sf $PWD/.tmux.conf ~
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
