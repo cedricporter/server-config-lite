048f73281e88cec8b50e4420b7865d3c774dbd16
Cedric Porter <cedricporter@gmail.com>
2018-05-29 10:39:28 +0800

    remove anaconda

--- init.sh
+++ init.sh
@@ -3,10 +3,15 @@
 ln -sf $PWD/.zshrc ~
 ln -sf $PWD/.tmux.conf ~
 
-mkdir ~/.pip
-ln -sf $PWD/pip.conf ~/.pip/
+# mkdir ~/.pip
+# ln -sf $PWD/pip.conf ~/.pip/
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-cd ~
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+# cd ~
+# wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
